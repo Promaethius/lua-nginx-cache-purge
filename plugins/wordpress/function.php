a82a0eb236af76fa3881f31652fbbaa1e7ecbda3
Promaethius <anon.clipsy.42@gmail.com>
2018-01-11 12:05:22 -0800

    Update function.php
    init

--- plugins/wordpress/function.php
+++ plugins/wordpress/function.php
@@ -1,17 +1,15 @@
 function my_project_updated_send_email( $post_id ) {
 
-	// If this is just a revision, don't send the email.
-	if ( wp_is_post_revision( $post_id ) )
-		return;
-
-	$post_title = get_the_title( $post_id );
 	$post_url = get_permalink( $post_id );
-	$subject = 'A post has been updated';
 
-	$message = "A post has been updated on your website:\n\n";
-	$message .= $post_title . ": " . $post_url;
+	$request = curl_init();
+	curl_setopt($request, CURLOPT_URL, $post_url);
+	curl_setopt($request, CURLOPT_CUSTOMREQUEST, "PURGE");
+	$error = curl_exec($request);
+	curl_close($request);
 
-	// Send email to admin.
-	wp_mail( 'admin@example.com', $subject, $message );
+	if ($error != nil) {
+		die($error);
+	}
 }
 add_action( 'save_post', 'my_project_updated_send_email' );
