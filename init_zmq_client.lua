d5df437bb2bcdcbc8a3c0079292bd5f0096c2d07
Promaethius <anon.clipsy.42@gmail.com>
2018-06-28 13:14:09 -0700

    Update init_zmq_client.lua

--- init_zmq_client.lua
+++ init_zmq_client.lua
@@ -1,9 +1,35 @@
 --[[
 init_by_lua_file /etc/nginx/lua-nginx-cache-purge/init_zmq_client.lua;
 
-# This module runs a lua worker that Subs to the zmq server and recieves commands to purge a particular uri.
+# This module runs two lua workers that Sub and Pub to the zmq server.
+# Pub:
+# By utilizing a shared dictionary, the Pub client will loop and push new DICT items to the server.
+# Sub: 
 ]]--
 
-if ngx != nil then
+require "zhelpers"
+local zmq      = require "lzmq"
+local zthreads = require "lzmq.threads"
+local ztimer   = require "lzmq.timer"
+local zpoller  = require "lzmq.poller"
 
+local include = [[
+	require "zhelpers"
+	local zmq      = require "lzmq"
+	local zthreads = require "lzmq.threads"
+	local ztimer   = require "lzmq.timer"
+	local zpoller  = require "lzmq.poller"
+]]
+
+local init_task = include .. [[
+	local ctx = zmq.context()
+]]
+
+local init_thread = include .. [[
+	local ctx = zthreads.get_parent_ctx()
+]]
+
+if ngx != nil then
+	zthreads.run(nil, pub_task):start(true)
+	zthreads.run(nil, sub_task):start(true)
 end
