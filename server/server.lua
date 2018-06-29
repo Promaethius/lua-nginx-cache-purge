00cdb2d1467720d9880fedfac0a871ab92f6fb65
Promaethius <anon.clipsy.42@gmail.com>
2018-06-29 11:51:48 -0700

    Delete server.lua
    gonna switch to a golang server

--- server/server.lua
+++
@@ -1,6 +0,0 @@
---[[
-Ran under LuaJIT2, this .lua provides a socket for the embedded nginx clients to send and receive information
-on what is being purged from the workers. It does not care who has what resource since it will assume nginx
-is either running with virtual server blocks or as a load balancer. Future work will include pub/sub to lists
-based on the nginx virtual server blocks.
-]]--
