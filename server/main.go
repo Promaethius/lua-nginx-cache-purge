2c1b9ddf40abd00fd661c5504112acc7d0c9968f
Promaethius <anon.clipsy.42@gmail.com>
2018-06-29 12:53:09 -0700

    Update main.go

--- server/main.go
+++ server/main.go
@@ -3,8 +3,25 @@
 package main
 
 import (
-    "fmt"
     zmq "github.com/pebbe/zmq4/"
     "strings"
-    "time"
 )
+
+// TODO: create subroutines for both the broker and publisher.
+
+func main() {    
+    // Create the broker socket.
+    broker, _ := zmq.NewSocket(zmq.ROUTER)
+    defer broker.Close()
+    broker.Bind("tcp://*:5600")
+    
+    // Create publisher socket.
+    publisher, _ := zmq.NewSocket(zmq.PUB)
+	defer publisher.Close()
+	publisher.Bind("tcp://*:5601")
+    
+    // Start MAIN control loop.
+    for {
+        
+    }
+}
